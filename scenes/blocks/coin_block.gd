05583fca40d55c74f5b2350b0d4a022a0deb6374
Matt <contact@maleero.com>
2016-04-10 17:41:54 -0700

    Used Robot.gd for mario

--- scenes/blocks/coin_block.gd
+++ scenes/blocks/coin_block.gd
@@ -7,22 +7,34 @@ const STATE_CLOSED = 2
 export var coin_value = 200
 export var coins = 1
 
+var bump_cooldown = 0
+
 var player_class = preload("res://scenes/player_mario/player_mario.gd")
 
 var state = null
 
-func _process(delta):
-	if (state == STATE_OPEN):
-		for body in self.get_colliding_bodies():
-			if body extends player_class:
+func _integrate_forces(s):
+	if state == STATE_OPEN:
+		for i in range(s.get_contact_count()):
+			var cc = s.get_contact_collider_object(i)
+			if cc extends player_class:
+				state = STATE_BUMPED
 				get_node("bounce").play("bump")
-			coins -= 1
-			if coins <= 0:
-				if get_node("block").get_frame() < 4:
+				coins -= 1
+				bump_cooldown = 0.25
+				if coins <= 0:
+				#	if get_node("block").get_frame() < 4:
 					get_node("single").stop_all()
 					get_node("block").set_frame(3)
-				state = STATE_CLOSED
-			break
+					state = STATE_CLOSED
+				break
+
+func _process(delta):
+	if state == STATE_BUMPED and bump_cooldown >= 0:
+		bump_cooldown -= delta
+		if bump_cooldown <= 0:
+			bump_cooldown = 0
+			state = STATE_OPEN
 
 func _ready():
 	set_process(true)
--- scenes/player_mario/player_mario.gd
+++ scenes/player_mario/player_mario.gd
@@ -1,31 +1,188 @@
 
-extends KinematicBody2D
+extends RigidBody2D
 
-const GRAVITY = 200.0
-const WALK_SPEED = 200
-var velocity = Vector2()
+# Character Demo, written by Juan Linietsky.
+#
+# Implementation of a 2D Character controller.
+# This implementation uses the physics engine for
+# controlling a character, in a very similar way
+# than a 3D character controller would be implemented.
+#
+# Using the physics engine for this has the main
+# advantages:
+# -Easy to write.
+# -Interaction with other physics-based objects is free
+# -Only have to deal with the object linear velocity, not position
+# -All collision/area framework available
+# 
+# But also has the following disadvantages:
+#  
+# -Objects may bounce a little bit sometimes
+# -Going up ramps sends the chracter flying up, small hack is needed.
+# -A ray collider is needed to avoid sliding down on ramps and  
+#   undesiderd bumps, small steps and rare numerical precision errors.
+#   (another alternative may be to turn on friction when the character is not moving).
+# -Friction cant be used, so floor velocity must be considered
+#  for moving platforms.
+
+# Member variables
+var anim = ""
+var siding_left = false
+var jumping = false
+var stopping_jump = false
+var shooting = false
+
+var WALK_ACCEL = 800.0
+var WALK_DEACCEL = 800.0
+var WALK_MAX_VELOCITY = 200.0
+var AIR_ACCEL = 1000.0
+var AIR_DEACCEL = 1000.0
+var JUMP_VELOCITY = 160
+var STOP_JUMP_FORCE = 900.0
+
+var MAX_FLOOR_AIRBORNE_TIME = 0.15
+
+var airborne_time = 1e20
+var shoot_time = 1e20
+
+var MAX_SHOOT_POSE_TIME = 0.3
+
+var floor_h_velocity = 0.0
+var enemy
 
 
-func _fixed_process(delta):
+func _integrate_forces(s):
+	var lv = s.get_linear_velocity()
+	var step = s.get_step()
+	
+	var new_anim = anim
+	var new_siding_left = siding_left
+	
+	# Get the controls
+	var move_left = Input.is_action_pressed("move_left")
+	var move_right = Input.is_action_pressed("move_right")
+	var jump = Input.is_action_pressed("jump")
+	
+	# Deapply prev floor velocity
+	lv.x -= floor_h_velocity
+	floor_h_velocity = 0.0
+	
+	# Find the floor (a contact with upwards facing collision normal)
+	var found_floor = false
+	var floor_index = -1
+	
+	for x in range(s.get_contact_count()):
+		var ci = s.get_contact_local_normal(x)
+		if (ci.dot(Vector2(0, -1)) > 0.6):
+			found_floor = true
+			floor_index = x
+	
+	# A good idea when impementing characters of all kinds,
+	# compensates for physics imprecission, as well as human reaction delay.
+	
+	if (found_floor):
+		airborne_time = 0.0
+	else:
+		airborne_time += step # Time it spent in the air
+	
+	var on_floor = airborne_time < MAX_FLOOR_AIRBORNE_TIME
 
-    velocity.y += delta * GRAVITY
-    if (Input.is_action_pressed("move_left")):
-        velocity.x = - WALK_SPEED
-    elif (Input.is_action_pressed("move_right")):
-        velocity.x =   WALK_SPEED
-    else:
-        velocity.x = 0
-
-    var motion = velocity * delta
-    motion = move(motion)
-
-	# handle sliding
-    if (is_colliding()):
-        var n = get_collision_normal()
-        motion = n.slide(motion)
-        velocity = n.slide(velocity)
-        move(motion)
+	# Process jump
+	if (jumping):
+		if (lv.y > 0):
+			# Set off the jumping flag if going down
+			jumping = false
+		elif (not jump):
+			stopping_jump = true
+		
+		if (stopping_jump):
+			lv.y += STOP_JUMP_FORCE*step
+	
+	if (on_floor):
+		# Process logic when character is on floor
+		if (move_left and not move_right):
+			if (lv.x > -WALK_MAX_VELOCITY):
+				lv.x -= WALK_ACCEL*step
+		elif (move_right and not move_left):
+			if (lv.x < WALK_MAX_VELOCITY):
+				lv.x += WALK_ACCEL*step
+		else:
+			var xv = abs(lv.x)
+			xv -= WALK_DEACCEL*step
+			if (xv < 0):
+				xv = 0
+			lv.x = sign(lv.x)*xv
+		
+		# Check jump
+		if (not jumping and jump):
+			lv.y = -JUMP_VELOCITY
+			jumping = true
+			stopping_jump = false
+			#get_node("sound").play("jump")
+		
+		# Check siding
+		if (lv.x < 0 and move_left):
+			new_siding_left = true
+		elif (lv.x > 0 and move_right):
+			new_siding_left = false
+		if (jumping):
+			new_anim = "jump"
+		elif (abs(lv.x) < 0.1):
+			new_anim = "idle"
+		else:
+			new_anim = "run"
+	else:
+		# Process logic when the character is in the air
+		if (move_left and not move_right):
+			if (lv.x > -WALK_MAX_VELOCITY):
+				lv.x -= AIR_ACCEL*step
+		elif (move_right and not move_left):
+			if (lv.x < WALK_MAX_VELOCITY):
+				lv.x += AIR_ACCEL*step
+		else:
+			var xv = abs(lv.x)
+			xv -= AIR_DEACCEL*step
+			if (xv < 0):
+				xv = 0
+			lv.x = sign(lv.x)*xv
+		
+		if (lv.y < 0):
+			new_anim = "jump"
+		else:
+			new_anim = "jump"
+	
+	# Update siding
+	if (new_siding_left != siding_left):
+		if (new_siding_left):
+			get_node("Sprite").set_scale(Vector2(-1, 1))
+		else:
+			get_node("Sprite").set_scale(Vector2(1, 1))
+		
+		siding_left = new_siding_left
+	
+	# Change animation
+	if (new_anim != anim):
+		anim = new_anim
+		get_node("Sprite/anim").play(anim)
+	
+	# Apply floor velocity
+	if (found_floor):
+		floor_h_velocity = s.get_contact_collider_velocity_at_pos(floor_index).x
+		lv.x += floor_h_velocity
+	
+	# Finally, apply gravity and set back the linear velocity
+	lv += s.get_total_gravity()*step
+	s.set_linear_velocity(lv)
 
 
 func _ready():
-	set_fixed_process(true)
+	pass
+	
+#	if !Globals.has_singleton("Facebook"):
+#		return
+#	var Facebook = Globals.get_singleton("Facebook")
+#	var link = Globals.get("facebook/link")
+#	var icon = Globals.get("facebook/icon")
+#	var msg = "I just sneezed on your wall! Beat my score and Stop the Running nose!"
+#	var title = "I just sneezed on your wall!"
+#	Facebook.post("feed", msg, title, link, icon)
